d4d8a0b41bc7b6fbe330b76fb00c6ed701969e56
Steve Hsu <stevehsu77@users.noreply.github.com>
2013-12-08 02:35:47 +0800

    Update service_server.dart

--- lib/core/service_server.dart
+++ lib/core/service_server.dart
@@ -1,9 +1,51 @@
-
 library yservice;
 
 import 'vm.dart';
 
 part 'service_translator.dart';
+
+// register a service
+// unregister a service
+// service heart beat (with or without status)
+// set a status
+// unset a status
+
+// require a service
+// respone a service
+// respone continue
+// cancel request
+
+// require collaboration
+// accept collaboration
+// deny collaboration
+// collaboration heart beat
+
+// announce a redirection
+// take a redirection
+// redirect a service
+// reject redirect
+
+// require update collaboration status
+// accept update collaboration status
+// deny update collaboration status
+// update collaboration status
+// parallel status updated
+
+/*
+service type:
+  pipe
+  hub
+  by_status   
+
+communication type:
+  socket
+  http
+  broadcast_stream
+  
+
+  
+
+*/
 
 class serviceServer extends VMbase
 {
